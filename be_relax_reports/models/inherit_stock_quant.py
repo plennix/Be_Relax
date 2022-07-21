594067cb6e9fe9a4dfb2d500f05088bfb191ea35
planet-odoo <sailee.haryan@planet-odoo.com>
2022-07-21 15:20:06 +0530

    commented the code for stocks.

--- be_relax_reports/models/inherit_stock_quant.py
+++ be_relax_reports/models/inherit_stock_quant.py
@@ -26,12 +26,12 @@ class InheritStockInventoryAdjust(models.TransientModel):
         print(self.quant_ids)
         for id in self.quant_ids.ids:
             self._cr.execute(f"""UPDATE stock_quant SET create_date='{self.forced_date}' WHERE id={id}""")
-            print("self.quant_ids.product_name", self.quant_ids.product_name)
-            print("self.quant_ids.default_code", self.quant_ids.default_code)
-            search_product = self.env['product.product'].search(['|', ('name', '=', self.quant_ids.product_name),
-                                                                 ('default_code', '=', self.quant_ids.product_name)])
-            search_prodcut_stock_valuation = self.env["stock.valuation.layer"].search([('product_id', '=', search_product.id)])
-            print(search_prodcut_stock_valuation)
-            stock_valuation = self.env["stock.valuation.layer"]
-            print(stock_valuation)
-            search_prodcut_stock_valuation._cr.execute(f"""UPDATE stock_valuation_layer SET create_date='{self.forced_date}' WHERE id={search_prodcut_stock_valuation.ids[-1]}""")
+            # print("self.quant_ids.product_name", self.quant_ids.product_name)
+            # print("self.quant_ids.default_code", self.quant_ids.default_code)
+            # search_product = self.env['product.product'].search(['|', ('name', '=', self.quant_ids.product_name),
+            #                                                      ('default_code', '=', self.quant_ids.product_name)])
+            # search_prodcut_stock_valuation = self.env["stock.valuation.layer"].search([('product_id', '=', search_product.id)])
+            # print(search_prodcut_stock_valuation)
+            # stock_valuation = self.env["stock.valuation.layer"]
+            # print(stock_valuation)
+            # search_prodcut_stock_valuation._cr.execute(f"""UPDATE stock_valuation_layer SET create_date='{self.forced_date}' WHERE id={search_prodcut_stock_valuation.ids[-1]}""")
