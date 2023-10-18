505465233b77c0875d890f76a9dfa035bd08433a
mahmoudfahmiebs <mahmoud.fahmi@plennix.com>
2023-10-18 10:27:22 +0400

    [CHNG]

--- point_of_sale_commission/static/src/js/Screens/ProductScreen/ProductScreen.js
+++ point_of_sale_commission/static/src/js/Screens/ProductScreen/ProductScreen.js
@@ -10,6 +10,16 @@ odoo.define('point_of_sale_commission.ProductScreen', function (require) {
     const ProductScreenAddEmp = (ProductScreen) =>
         class extends ProductScreen {
             async _clickProduct(event) {
+
+                if (!this.partner) {
+                // prevent adding product if customer not selected
+                      this.showPopup('ErrorPopup', {
+                    title: this.env._t('Error'),
+                    body: this.env._t('Please select a customer first to add product!'),
+                });
+                return
+                }
+
                 if (!this.currentOrder) {
                     this.env.pos.add_new_order();
                 }
@@ -17,6 +27,8 @@ odoo.define('point_of_sale_commission.ProductScreen', function (require) {
             const options = await this._getAddProductOptions(product);
             if (!options) return;
             if (this.env.pos.employees){
+                var self = this;
+                const cashier = this.env.pos.cashier
                 const employeesList = this.env.pos.employees
                         .map((employee) => {
                             return {
@@ -27,9 +39,16 @@ odoo.define('point_of_sale_commission.ProductScreen', function (require) {
                                 isSelected: false,
                             };
                         });
+                const other_employeesList = employeesList.filter(function (ol) {
+                        return ol.id != cashier.id;
+                    });
+                const cashier_employee = employeesList.filter(function (ol) {
+                        return ol.id == cashier.id;
+                    });
+                const sorted_employee = cashier_employee.concat(other_employeesList)
                 let {confirmed, payload: employee} = await this.showPopup('SelectionPopup', {
                     title: this.env._t('Therapist'),
-                    list: employeesList,
+                    list: sorted_employee,
                 });
                 if(confirmed){
                     options['employee_id']=employee.id;
@@ -39,6 +58,29 @@ odoo.define('point_of_sale_commission.ProductScreen', function (require) {
             await this._addProduct(product, options);
             NumberBuffer.reset();
         }
+            async _onClickPay() {
+
+                // prevent proceeding with payment if customer not selected and products added
+                if(!this.partner){
+                     this.showPopup('ErrorPopup', {
+                    title: this.env._t('Error'),
+                    body: this.env._t('Please select a customer first to add product!'),
+                });
+                    return
+                }
+
+                const { confirmed, payload } = await this.showPopup('ConfirmPopup', {
+                    'title': this.env._t('Add Product'),
+                    'body':  this.env._t('Add more products?'),
+                });
+
+                if (confirmed) {
+                    this.showScreen('ProductScreen');
+                }else{
+//                    this.showScreen('PaymentScreen');
+                    super._onClickPay();
+                }
+            }
         };
 
     Registries.Component.extend(ProductScreen, ProductScreenAddEmp);
