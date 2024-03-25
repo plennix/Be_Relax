9e39ed67aca977b740a89a7c9bcb6adc02f56b6a
plennix <mahmoud.fahmi@plennix.com>
2024-03-25 15:19:48 +0400

    [UPD]

--- po_receipt_restriction/models/purchase_order.py
+++ po_receipt_restriction/models/purchase_order.py
@@ -3,7 +3,7 @@ from odoo import fields, models, api,_
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    create_receipt = fields.Boolean("Create Receipt")
+    create_receipt = fields.Boolean("Create Receipt", default=True)
 
     def _create_picking(self):
         if self.create_receipt:
