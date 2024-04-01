ea35a05824e67136557f7af7ae51c0144ee1accf
Tirth <tirthd.techultra@gmail.com>
2024-04-01 12:01:25 +0530

    [IMP] (1) Add the same therapists selected in original pos order to  refund order lines rather than defaulting to the POS cashier user, (2) Show emp_line_pin (barcode) in POS screen orderline for refund order

--- point_of_sale_commission/models/pos_order.py
+++ point_of_sale_commission/models/pos_order.py
@@ -11,8 +11,13 @@ class PosLineAddEmp(models.Model):
             po_line = self.env['pos.order.line'].sudo().search([('order_id', '=', po_order.get('id'))])
             for line in po_line:
                 if not line.employee_id:
-                    line.employee_id = self.env.user.employee_id.id
+                    # Take therapist from related refunded pos orderline
+                    if line.refunded_orderline_id and line.refunded_orderline_id.employee_id:
+                        line.employee_id = line.refunded_orderline_id.employee_id.id
+                    else:
+                        line.employee_id = self.env.user.employee_id.id
         return order_ids
+
 
 class PosOrderLineEmp(models.Model):
     _inherit = 'pos.order.line'
@@ -22,3 +27,19 @@ class PosOrderLineEmp(models.Model):
         string='Therapist',
         required=False
     )
+    line_emp_pin = fields.Char(related='employee_id.barcode', store=True)
+
+    def _export_for_ui(self, orderline):
+        res = super()._export_for_ui(orderline=orderline)
+        if orderline.employee_id:
+            res['employee_id'] = orderline.employee_id.id
+        if orderline.line_emp_pin:
+            res['line_emp_pin'] = orderline.line_emp_pin
+        return res
+
+    def get_emp_line_emp_pin(self):
+        employee_id = self.employee_id
+        line_emp_pin = False
+        if employee_id.barcode:
+            line_emp_pin = int(employee_id.barcode)
+        return [employee_id.id, line_emp_pin]
--- point_of_sale_commission/static/src/js/models.js
+++ point_of_sale_commission/static/src/js/models.js
@@ -36,10 +36,44 @@ const PosSaleOrderEmp = (Order) => class PosSaleOrderEmp extends Order {
 
     set_orderline_options(line, options) {
         super.set_orderline_options(...arguments);
+        debugger;
         if (options.employee_id || options.line_emp_pin){
             line.set_line_emp(options.employee_id,options.line_emp_pin)
         }
+        else if(options.refunded_orderline_id && !line.employee_id && !line.line_emp_pin){
+            const self = this
+            var data = this.pos.env.services.rpc({
+                    model: 'pos.order.line',
+                    method: 'get_emp_line_emp_pin',
+                    args: [parseInt(options.refunded_orderline_id)],
+                });
+            if (data && (data[0] || data[1])) {
+                const employee_id = data[0]
+                const line_emp_pin = data[1]
+                line.set_line_emp(employee_id,line_emp_pin)
+            }
+        }
      }
+
+     select_orderline(line) {
+        super.select_orderline(...arguments);
+        debugger;
+        if (line && line.refunded_orderline_id && typeof line.refunded_orderline_id === 'number' && !line.employee_id && !line.line_emp_pin){
+            const self = this
+            this.pos.env.services.rpc({
+                    model: 'pos.order.line',
+                    method: 'get_emp_line_emp_pin',
+                    args: [parseInt(line.refunded_orderline_id)],
+                }).then(function(data){
+                    debugger;
+                    if (data && (data[0] || data[1])) {
+                        const employee_id = data[0]
+                        const line_emp_pin = data[1]
+                        line.set_line_emp(employee_id,line_emp_pin)
+                    }
+                 });
+        }
+    }
 }
 Registries.Model.extend(Order, PosSaleOrderEmp);
 
